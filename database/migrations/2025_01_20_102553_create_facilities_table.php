6b92dbbae2d060b226a85c5be35428a6e5e6a68a
hanep77 <yudishan26@gmail.com>
2025-01-25 13:09:31 +0700

    fix name typo in migration

--- database/migrations/2025_01_20_102553_create_facilities_table.php
+++ database/migrations/2025_01_20_102553_create_facilities_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
     {
         Schema::create('facilities', function (Blueprint $table) {
             $table->id();
-            $table->string("rule");
+            $table->string("name");
             $table->text("description")->nullable();
             $table->timestamps();
         });
